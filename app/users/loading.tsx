6461d21840db287d77126d7bfca0cc9adf313140
Varun <aitvarunbhatia@gmail.com>
2024-06-27 12:46:13 +0530

    updated loader

--- app/users/loading.tsx
+++ app/users/loading.tsx
@@ -1,4 +1,11 @@
 export default function Loading() {
   // Or a custom loading skeleton component
-  return <p>Loading Users Page...</p>;
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <p className="text-gray-900 text-xl mb-4">Loading...</p>
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+      </div>
+    </>
+  );
 }
