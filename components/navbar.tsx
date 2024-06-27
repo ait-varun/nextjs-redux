8294da0993ea1e4a732cd2798f1c38f18202b793
Varun <aitvarunbhatia@gmail.com>
2024-06-27 15:15:10 +0530

    updated navbar position

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,7 +28,7 @@ export default function Navbar() {
   return (
     <>
       {/* Desktop Navigation */}
-      <div className="hidden lg:grid  grid-cols-2 lg:grid-cols-3 justify-end gap-4 items-center">
+      <div className="hidden lg:grid  grid-cols-2 lg:grid-cols-3 justify-end gap-4 items-center sticky top-0 z-50 bg-white">
         <div className="grid place-content-start ms-6 my-2">App Icon</div>
         <NavigationMenu className="mx-auto max-w-screen-sm  ">
           <NavigationMenuList>
@@ -75,7 +75,7 @@ export default function Navbar() {
       </div>
 
       {/* Mobile Navigation */}
-      <nav className=" lg:hidden">
+      <nav className=" fixed top-0 left-0 right-0 z-50 lg:hidden bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
