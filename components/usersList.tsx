15fc182945b5e3e26300b949ebc7530389d3e445
Varun <aitvarunbhatia@gmail.com>
2024-06-27 11:13:48 +0530

    updated code

--- components/usersList.tsx
+++ components/usersList.tsx
@@ -43,9 +43,6 @@ export default function UsersList() {
 
   return (
     <div className="container mx-auto py-8">
-      <div className="mb-4 text-lg font-semibold">
-        Total Users: {data?.limit}
-      </div>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
         {combinedUsers.map((user) => (
           <li
