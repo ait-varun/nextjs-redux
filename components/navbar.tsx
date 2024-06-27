a97de81a3ab176473ef8880c542f9491b296bd1a
Varun <aitvarunbhatia@gmail.com>
2024-06-27 15:31:17 +0530

    updated css

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -75,7 +75,7 @@ export default function Navbar() {
       </div>
 
       {/* Mobile Navigation */}
-      <nav className=" fixed top-0 left-0 right-0 z-50 lg:hidden bg-white">
+      <nav className="sticky top-0 left-0 right-0 z-50 lg:hidden bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
